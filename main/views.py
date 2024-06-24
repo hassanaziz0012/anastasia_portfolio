37e88d715403e9d223ccacf41f2d2fb4246d93ba
Hassan Aziz <hassanaziz0012@gmail.com>
2024-06-24 11:48:48 +0500

    Initial Commit

---
+++ main/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.views import View, generic
+
+# Create your views here.
+class IndexView(generic.TemplateView):
+    template_name = "index.html"
+    
