7f7ceb31cf978f8d993ff6d5d1635274b698952f
Hassan Aziz <hassanaziz0012@gmail.com>
2024-06-25 19:57:30 +0500

    changes

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from main.views import IndexView
+from main.views import CommunicationManagementWorkView, IndexView, ResearchAndCertificationsWorkView, UGCWorkView
 
 urlpatterns = [
-    path("", IndexView.as_view(), name="index")
+    path("", IndexView.as_view(), name="index"),
+    path("work/ugc", UGCWorkView.as_view(), name="ugc-work"),
+    path("work/communication-management", CommunicationManagementWorkView.as_view(), name="communication-management-work"),
+    path("work/research-and-certifications", ResearchAndCertificationsWorkView.as_view(), name="research-certifications-work"),
 ]
--- main/views.py
+++ main/views.py
@@ -4,4 +4,15 @@ from django.views import View, generic
 # Create your views here.
 class IndexView(generic.TemplateView):
     template_name = "index.html"
-    
+
+
+class UGCWorkView(generic.TemplateView):
+    template_name = "work/ugc/index.html"
+
+
+class CommunicationManagementWorkView(generic.TemplateView):
+    template_name = "work/communication_management/index.html"
+
+
+class ResearchAndCertificationsWorkView(generic.TemplateView):
+    template_name = "work/research_and_certifications/index.html"
